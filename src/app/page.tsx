318a15a98f4bf458ab62ba7a343ad825256a78c0
jkojobekov <zhakshylykkozhobekov@gmail.com>
2025-09-09 15:01:26 +0600

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
-import { Categories, NewProducts, PopularProducts } from "@/features";
-import { Banner, FeaturesBlock } from "@/widgets/home";
+import {Categories, NewProducts, PopularProducts} from "@/features";
+import {Banner, FeaturesBlock} from "@/widgets/home";
 
 export default function Home() {
   return (
     <>
-       <Banner/>
-       <Categories/>
-       <NewProducts />
-      <FeaturesBlock />
-      <PopularProducts />
+      <Banner/>
+      <Categories/>
+      <NewProducts/>
+      <FeaturesBlock/>
+      <PopularProducts/>
     </>
   );
 }
